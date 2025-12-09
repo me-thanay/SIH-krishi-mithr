9737b010a57c4a52223d9535802b91041eb27d6f
thanay <thanayp99@gmail.com>
2025-12-09 14:34:59 +0530

    Remove auth pages and auth API routes

--- pages/api/auth/signup.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Auth disabled
-  return res.status(410).json({ error: 'Signup is disabled' })
-}
